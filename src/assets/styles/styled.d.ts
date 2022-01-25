118bea70ba5ec58a34d470f7e78e20982a38a50a
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2022-01-25 08:53:50 +0100

    feat: Add themes and globalStyles

---
+++ src/assets/styles/styled.d.ts
@@ -0,0 +1,54 @@
+// import original module declarations
+import "styled-components";
+
+export type Theme = "light" | "dark";
+// and extend them!
+declare module "styled-components" {
+  export interface DefaultTheme {
+    mode: Theme;
+
+    breakpoints: {
+      m: string;
+      l: string;
+      xl: string;
+    };
+
+    colors: {
+      primary: {
+        300: string;
+        500: string;
+        700: string;
+      };
+      neutral: {
+        100: string;
+        200: string;
+        300: string;
+        500: string;
+        600: string;
+        700: string;
+        900: string;
+      };
+      success: {
+        100: string;
+        300: string;
+        500: string;
+        700: string;
+        900: string;
+      };
+      warning: {
+        100: string;
+        300: string;
+        500: string;
+        700: string;
+        900: string;
+      };
+      error: {
+        100: string;
+        300: string;
+        500: string;
+        700: string;
+        900: string;
+      };
+    };
+  }
+}
