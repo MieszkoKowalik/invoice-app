af51db1af5cc3703e61ec518be7252cee1ad915f
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2022-01-26 20:34:10 +0100

    feat: Add Input component

---
+++ src/components/atoms/Input/Input.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  padding: 16px 20px;
+  width: 100%;
+  border-radius: 4px;
+  border: 1px solid ${({ theme }) => theme.colors.neutral[300]};
+  background-color: ${({ theme }) => theme.colors.neutral[200]};
+  color: ${({ theme }) => theme.colors.neutral[900]};
+  font-size: 0.9375rem;
+  font-weight: 700;
+  caret-color: ${({ theme }) => theme.colors.primary[500]};
+  &:focus {
+    outline: none;
+    border-color: ${({ theme }) => theme.colors.primary[500]};
+  }
+  &:invalid {
+    border-color: ${({ theme }) => theme.colors.error[500]};
+  }
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.neutral[900]};
+    opacity: 0.6;
+  }
+`;
