d16b4a76c7c984e9493e0121d9d462953efe6eb8
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2022-01-29 10:43:39 +0100

    feat: Add Button component

---
+++ src/components/atoms/Button/Button.stories.tsx
@@ -0,0 +1,29 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Button } from "./Button";
+
+export default {
+  title: "components/atoms/Button",
+  component: Button,
+  args: {
+    children: "Click me",
+  },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  variant: "primary",
+};
+export const Secondary = Template.bind({});
+Secondary.args = {
+  variant: "secondary",
+};
+export const Danger = Template.bind({});
+Danger.args = {
+  variant: "danger",
+};
+export const Bordered = Template.bind({});
+Bordered.args = {
+  variant: "bordered",
+};
