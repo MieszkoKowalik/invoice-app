658bffcb2c3e0b83882db719db861d6550494820
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2022-01-30 13:46:39 +0100

    test: Add matchMedia mocking for tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,4 +2,35 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+const defaultMediaQueriesValue = {
+  default: false,
+};
+
+let mediaQueries: { [key: string]: boolean } = defaultMediaQueriesValue;
+
+const getMediaMatches = (query: string) => {
+  return mediaQueries[query] || mediaQueries.default;
+};
+
+export const setMediaMatches = (query: string, value: boolean) => {
+  mediaQueries = { ...mediaQueries, [query]: value };
+};
+
+beforeEach(() => {
+  mediaQueries = defaultMediaQueriesValue;
+  return Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: (query: string) => ({
+      matches: getMediaMatches(query),
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // Deprecated
+      removeListener: jest.fn(), // Deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    }),
+  });
+});
