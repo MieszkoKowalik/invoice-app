c5152993cc30d18053ad2fc5a12907b8907fdcb0
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2022-02-01 21:08:36 +0100

    feat: Add sign out button

--- src/components/organisms/Navbar/Navbar.tsx
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -3,9 +3,12 @@ import { Link } from "react-router-dom";
 import { Wrapper, LogoWrapper, UserWrapper, Panel } from "./Navbar.styles";
 import UserImage from "components/molecules/UserImage/UserImage";
 import ThemeToggler from "components/molecules/ThemeToggler/ThemeToggler";
+import { useAuth } from "providers/AuthProvider";
 type Props = {};
 
 const Navbar = (props: Props) => {
+  const { user, logOut } = useAuth();
+
   return (
     <Wrapper>
       <LogoWrapper>
@@ -15,6 +18,11 @@ const Navbar = (props: Props) => {
       </LogoWrapper>
       <Panel>
         <ThemeToggler />
+        {user ? (
+          <button type="button" onClick={logOut}>
+            Sign Out
+          </button>
+        ) : null}
         <UserWrapper>
           <UserImage />
         </UserWrapper>
