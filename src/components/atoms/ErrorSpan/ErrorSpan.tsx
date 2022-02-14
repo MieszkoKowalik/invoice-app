c0c2aee64659bb531314a4e0327fe140e1315217
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2022-02-14 18:41:52 +0100

    fix: Change ErrorSpan color

--- src/components/atoms/ErrorSpan/ErrorSpan.tsx
+++ src/components/atoms/ErrorSpan/ErrorSpan.tsx
@@ -3,5 +3,5 @@ import styled from "styled-components";
 export const ErrorSpan = styled.span`
   font-size: ${({ theme }) => theme.fontSize.xs};
   font-weight: 600;
-  color: ${({ theme }) => theme.colors.danger[500]};
+  color: ${({ theme }) => theme.colors.danger[700]};
 `;
