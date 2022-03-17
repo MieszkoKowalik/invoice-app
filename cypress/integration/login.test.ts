8028daf6959cf70245b81ebb7cbc9a0920b11990
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2022-03-17 22:37:44 +0100

    test: Add login tests

---
+++ cypress/integration/login.test.ts
@@ -0,0 +1,52 @@
+describe("login", () => {
+  beforeEach(() => {
+    indexedDB.deleteDatabase("firebaseLocalStorageDb");
+  });
+
+  it("Displays login page", () => {
+    cy.visit("/");
+    cy.findByText("Log in").should("exist");
+  });
+
+  it("Shows errors when invalid data provided", () => {
+    cy.visit("/");
+    cy.findByText("Log in").click();
+    cy.findAllByText("Can't be empty").should("have.length", 2);
+
+    cy.findByLabelText(/Email/).type("Test123@test123.com");
+    cy.findByLabelText(/Password/).type("WrongPassword");
+    cy.findByText("Log in").click();
+    cy.findByText("Wrong password").should("exist");
+
+    cy.findByLabelText(/Email/).clear();
+    cy.findByLabelText(/Password/).clear();
+    cy.findByLabelText(/Email/).type("WrongEmail@test123.com");
+    cy.findByLabelText(/Password/).type("Test123");
+    cy.findByText("Log in").click();
+    cy.findByText("We couldn't find an account conected to this email.").should(
+      "exist"
+    );
+
+    cy.intercept(
+      { method: "POST", url: "**" },
+      {
+        statusCode: 400,
+        body: {
+          message: "Error",
+        },
+      }
+    );
+    cy.findByText("Log in").click();
+    cy.findByText("We were unable to log you in, please try again").should(
+      "exist"
+    );
+  });
+
+  it("Logins user when valid data provided", () => {
+    cy.visit("/");
+    cy.findByLabelText("Email").type("Test123@test123.com");
+    cy.findByLabelText("Password").type("Test123");
+    cy.findByText("Log in").click();
+    cy.findByText("Invoices").should("exist");
+  });
+});
