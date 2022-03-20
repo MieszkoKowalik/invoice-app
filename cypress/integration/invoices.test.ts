8df8f0d8a32636553922ca5d64719c4707067148
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2022-03-20 21:37:40 +0100

    test: Add form validation test on invoice adding

--- cypress/integration/invoices.test.ts
+++ cypress/integration/invoices.test.ts
@@ -11,4 +11,11 @@ describe("login", () => {
     cy.findByText("save as draft").click();
     cy.findByText("There is 1 invoice").should("exist");
   });
+
+  it("Shows form errors on 'save & send' click", () => {
+    cy.findByRole("button", { name: "New Invoice" }).click();
+    cy.findByText("save & send").click();
+    cy.findAllByText("Can't be empty").should("have.length", 12);
+    cy.findByText("An item must be added").should("exist");
+  });
 });
