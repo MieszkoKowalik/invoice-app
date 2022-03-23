a77e74f668bff39b6f7fd9076869647fa0fda2ce
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2022-03-23 19:23:03 +0100

    feat: Add InvoiceList animations

--- src/components/organisms/InvoiceList/InvoiceList.tsx
+++ src/components/organisms/InvoiceList/InvoiceList.tsx
@@ -2,23 +2,52 @@ import { Invoice } from "types";
 import InvoiceCard from "components/molecules/InvoiceListCard/InvoiceCard";
 import { StyledUl, StyledLink } from "./InvoiceList.styles";
 import EmptyList from "components/molecules/EmptyList/EmptyList";
+import { motion } from "framer-motion";
 
 type InvoiceListProps = {
   invoices: Invoice[];
+};
+
+const list = {
+  hidden: {
+    opacity: 0,
+    transition: {
+      staggerChildren: 0.02,
+      staggerDirection: -1,
+      when: "afterChildren",
+    },
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const item = {
+  hidden: { opacity: 0, y: -20 },
+  show: { opacity: 1, y: 0 },
 };
 
 const InvoiceList = ({ invoices }: InvoiceListProps) => {
   return (
     <>
       {invoices.length ? (
-        <StyledUl>
+        <StyledUl
+          as={motion.ul}
+          variants={list}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+        >
           {invoices.map((invoice: Invoice) => {
             return (
-              <li key={invoice.id}>
+              <motion.li key={invoice.id} variants={item}>
                 <StyledLink to={`invoice/${invoice.id}`}>
                   <InvoiceCard invoice={invoice} />
                 </StyledLink>
-              </li>
+              </motion.li>
             );
           })}
         </StyledUl>
