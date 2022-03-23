0daab8ace8d554815a55e67806e0f659f7b80543
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2022-03-23 18:54:06 +0100

    feat: Add Link to single invoice view

--- src/components/organisms/InvoiceList/InvoiceList.styles.tsx
+++ src/components/organisms/InvoiceList/InvoiceList.styles.tsx
@@ -1,8 +1,13 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const StyledUl = styled.ul`
   display: flex;
   flex-direction: column;
   gap: 16px;
   list-style: none;
+`;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
 `;
--- src/components/organisms/InvoiceList/InvoiceList.tsx
+++ src/components/organisms/InvoiceList/InvoiceList.tsx
@@ -1,6 +1,6 @@
 import { Invoice } from "types";
 import InvoiceCard from "components/molecules/InvoiceListCard/InvoiceCard";
-import { StyledUl } from "./InvoiceList.styles";
+import { StyledUl, StyledLink } from "./InvoiceList.styles";
 import EmptyList from "components/molecules/EmptyList/EmptyList";
 
 type InvoiceListProps = {
@@ -15,7 +15,9 @@ const InvoiceList = ({ invoices }: InvoiceListProps) => {
           {invoices.map((invoice: Invoice) => {
             return (
               <li key={invoice.id}>
-                <InvoiceCard invoice={invoice}></InvoiceCard>
+                <StyledLink to={`invoice/${invoice.id}`}>
+                  <InvoiceCard invoice={invoice} />
+                </StyledLink>
               </li>
             );
           })}
