a997b33338e50e26093da6764bdff5c764cbc7ad
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2022-03-23 18:58:09 +0100

    fix: Setup FramerMotion to properly handle route change

--- src/views/Root.tsx
+++ src/views/Root.tsx
@@ -1,15 +1,17 @@
 import MainTemplate from "components/templates/MainTemplate/MainTemplate";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Dashboard from "./Dashboard/Dashboard";
 import Login from "./Login/Login";
 import RequireAuth from "helpers/RequireAuth";
 import { AnimatePresence } from "framer-motion";
 
 const Root = () => {
+  const location = useLocation();
+
   return (
     <MainTemplate>
       <AnimatePresence exitBeforeEnter>
-        <Routes>
+        <Routes location={location} key={location.pathname}>
           <Route path="/login" element={<Login />} />
           <Route
             path="/"
