58fa2f938fad7fd5387425df86bbb3d6c51ff442
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2022-03-23 18:26:46 +0100

    feat: Add AnimatePresence wrapper for Routes

--- src/views/Root.tsx
+++ src/views/Root.tsx
@@ -3,21 +3,24 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from "./Dashboard/Dashboard";
 import Login from "./Login/Login";
 import RequireAuth from "helpers/RequireAuth";
+import { AnimatePresence } from "framer-motion";
 
 const Root = () => {
   return (
     <MainTemplate>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/"
-          element={
-            <RequireAuth>
-              <Dashboard />
-            </RequireAuth>
-          }
-        />
-      </Routes>
+      <AnimatePresence exitBeforeEnter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <RequireAuth>
+                <Dashboard />
+              </RequireAuth>
+            }
+          />
+        </Routes>
+      </AnimatePresence>
     </MainTemplate>
   );
 };
