f4e5c52f4c13b3962116e7bd74f1174c5423e517
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2022-03-23 17:35:05 +0100

    refactor: Move providers to AppProviders

--- src/views/Root.tsx
+++ src/views/Root.tsx
@@ -1,32 +1,24 @@
 import MainTemplate from "components/templates/MainTemplate/MainTemplate";
-import ThemesProvider from "providers/ThemesProvider";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AuthProvider from "providers/AuthProvider";
+import { Routes, Route } from "react-router-dom";
 import Dashboard from "./Dashboard/Dashboard";
 import Login from "./Login/Login";
 import RequireAuth from "helpers/RequireAuth";
 
 const Root = () => {
   return (
-    <BrowserRouter>
-      <ThemesProvider>
-        <AuthProvider>
-          <MainTemplate>
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route
-                path="/"
-                element={
-                  <RequireAuth>
-                    <Dashboard />
-                  </RequireAuth>
-                }
-              />
-            </Routes>
-          </MainTemplate>
-        </AuthProvider>
-      </ThemesProvider>
-    </BrowserRouter>
+    <MainTemplate>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <Dashboard />
+            </RequireAuth>
+          }
+        />
+      </Routes>
+    </MainTemplate>
   );
 };
 
