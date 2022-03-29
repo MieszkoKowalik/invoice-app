16603a182de708d397d87710ba7af4d123532d44
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2022-03-29 17:13:11 +0200

    feat: Add ContentWrapper component

---
+++ src/components/molecules/ContentWrapper/ContentWrapper.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const ContentWrapper = styled.div`
+  padding: 24px;
+  background-color: ${({ theme }) => theme.colors.neutral[50]};
+  border-radius: 8px;
+`;
